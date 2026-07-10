80dc72496629c5393ddcc498297f23e7ed8bf419
santakameow <sakanai@cum.army>
2026-07-10 20:10:17 +0300

    replace code to handler example

--- main.go
+++ main.go
@@ -6,24 +6,52 @@ import (
 	"os"
 
 	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
+	tu "github.com/mymmrac/telego/telegoutil"
 )
 
 func main() {
-	// get bot token from env
-	botToken := os.Getenv("BOT_TOKEN")
+	ctx := context.Background()
+	botToken := os.Getenv("TOKEN")
 
-	// create bot
+	// Note: Please keep in mind that default logger may expose sensitive information, use in development only
 	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// call method getMe
-	botUser, err := bot.GetMe(context.Background())
-	if err != nil {
-		fmt.Println("error: ", err)
-	}
+	// Get updates channel
+	updates, _ := bot.UpdatesViaLongPolling(ctx, nil)
 
-	fmt.Printf("bot user: %+v\n", botUser)
+	// Create bot handler and specify from where to get updates
+	bh, _ := th.NewBotHandler(bot, updates)
+
+	// Register new handler with match on command `/start`
+	bh.Handle(func(ctx *th.Context, update telego.Update) error {
+		// Send message
+		_, _ = bot.SendMessage(ctx, tu.Messagef(
+			tu.ID(update.Message.Chat.ID),
+			"Hello %s!", update.Message.From.FirstName,
+		))
+		return nil
+	}, th.CommandEqual("start"))
+
+	// Register new handler with match on any command
+	// Handlers will match only once and in order of registration, so this handler will be called on any command except
+	// `/start` command
+	bh.Handle(func(ctx *th.Context, update telego.Update) error {
+		// Send message
+		_, _ = bot.SendMessage(ctx, tu.Message(
+			tu.ID(update.Message.Chat.ID),
+			"Unknown command, use /start",
+		))
+		return nil
+	}, th.AnyCommand())
+
+	// Stop handling updates
+	defer func() { _ = bh.Stop() }()
+
+	// Start handling updates
+	_ = bh.Start()
 }

155284cfb5667901076eab4cacb53d59d1899733
santakameow <sakanai@cum.army>
2026-07-11 14:00:52 +0300

    1/3 tasks done, now bot can send messages

--- main.go
+++ main.go
@@ -27,27 +27,11 @@ func main() {
 	// Create bot handler and specify from where to get updates
 	bh, _ := th.NewBotHandler(bot, updates)
 
-	// Register new handler with match on command `/start`
+	// bothandler
 	bh.Handle(func(ctx *th.Context, update telego.Update) error {
-		// Send message
-		_, _ = bot.SendMessage(ctx, tu.Messagef(
-			tu.ID(update.Message.Chat.ID),
-			"Hello %s!", update.Message.From.FirstName,
-		))
+		bot.SendMessage(ctx, tu.Message(tu.ID(update.Message.Chat.ID), "meow"))
 		return nil
-	}, th.CommandEqual("start"))
-
-	// Register new handler with match on any command
-	// Handlers will match only once and in order of registration, so this handler will be called on any command except
-	// `/start` command
-	bh.Handle(func(ctx *th.Context, update telego.Update) error {
-		// Send message
-		_, _ = bot.SendMessage(ctx, tu.Message(
-			tu.ID(update.Message.Chat.ID),
-			"Unknown command, use /start",
-		))
-		return nil
-	}, th.AnyCommand())
+	})
 
 	// Stop handling updates
 	defer func() { _ = bh.Stop() }()

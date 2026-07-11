9cc43e09b7be569aaf0149ed3a8f5eff5bbe1929
Sleroq <hireme@sleroq.link>
2026-07-11 15:50:25 +0300

    Support for more systems and package definition

--- flake.nix
+++ flake.nix
@@ -5,18 +5,48 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShells.${system}.default = pkgs.mkShell.override {
-    } {
-      packages = with pkgs; [
-        go
-        gopls
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+            ];
+          };
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "NeRugaysyaBot";
+            version = "0.0.1";
+            src = ./.;
+            vendorHash = "sha256-pa187egetlojL783dc/04UWGWkLZbC0jTW7fj4P1bzk=";
+            buildInputs = [ ];
+          };
+        }
+      );
     };
-  };
 }
